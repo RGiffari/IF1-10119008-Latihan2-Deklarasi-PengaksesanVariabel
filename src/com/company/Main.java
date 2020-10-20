4a826c2d247a74c661d51e29e72454843684aec4
RGiffari <11R02Giffari@users.noreply.github.com>
2020-10-20 23:39:35 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+/**
+ *
+ * @author
+ * NAMA         : Raihan Giffari
+ * KELAS        : PBO12
+ * NIM          : 10119008
+ * Deskripsi Program    : Program ini berisi tentang beberapa penggunaan variabel
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+    // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+    // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+}
